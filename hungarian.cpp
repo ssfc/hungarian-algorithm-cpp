7e9f729c94ba6ed467d861927d38cde27b0b8b1e
SSFC <sortsimilar@gmail.com>
2024-03-09 21:20:35 +0800

    replace 3

--- hungarian.cpp
+++ hungarian.cpp
@@ -31,6 +31,7 @@ Hungarian::Hungarian(const std::string &input_filename):
         }
 
         // test cost input
+        /*
         for (int i = 0; i < num_rows; i++)
         {
             for (int j = 0; j < num_columns; j++)
@@ -38,7 +39,7 @@ Hungarian::Hungarian(const std::string &input_filename):
                 std::cout << cost_matrix[i][j] << " ";
             }
             std::cout << std::endl;
-        }
+        }*/
 
         // (0, 3), (2, 4), (3, 0), (5, 2), (1, 5), (4, 1): 第0个工人分配了第3个任务, 第2个工人分配了第4个任务, 以此类推。
         // cout << cost_matrix[0][3] + cost_matrix[2][4] + cost_matrix[3][0] + cost_matrix[5][2] + cost_matrix[1][5]
@@ -67,7 +68,7 @@ void Hungarian::brute_force()
     do
     {
         int current_cost = 0;
-        for(int i = 0; i < 3; i++)
+        for(int i = 0; i < num_rows; i++)
         {
             current_cost += cost_matrix[i][job_assignment[i]];
         }
@@ -81,7 +82,8 @@ void Hungarian::brute_force()
 
     std::cout << "min cost: " << min_cost << std::endl;
     std::cout << "assign: " << std::endl;
-    for(int i = 0; i < 3; i++)
+
+    for(int i = 0; i < num_rows; i++)
     {
         std::cout << "worker " << i << " assignment " << best_job_assignment[i] << std::endl;
     }
