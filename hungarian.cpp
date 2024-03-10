6782245ac625845742a39584d020c78e304be169
SSFC <sortsimilar@gmail.com>
2024-03-10 10:14:42 +0800

    test

--- hungarian.cpp
+++ hungarian.cpp
@@ -60,7 +60,7 @@ Hungarian::Hungarian(const std::string &input_filename):
         }
 
         // test cost input
-        /*
+
         for (int i = 0; i < num_rows; i++)
         {
             for (int j = 0; j < num_columns; j++)
@@ -68,7 +68,8 @@ Hungarian::Hungarian(const std::string &input_filename):
                 std::cout << cost_matrix[i][j] << " ";
             }
             std::cout << std::endl;
-        }*/
+        }
+        //*/
 
         // (0, 3), (2, 4), (3, 0), (5, 2), (1, 5), (4, 1): 第0个工人分配了第3个任务, 第2个工人分配了第4个任务, 以此类推。
         // cout << cost_matrix[0][3] + cost_matrix[2][4] + cost_matrix[3][0] + cost_matrix[5][2] + cost_matrix[1][5]
