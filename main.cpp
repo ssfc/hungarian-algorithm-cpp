16f275494e5b47b069f8b3e57048b0c03e4ee18e
SSFC <sortsimilar@gmail.com>
2024-04-08 10:15:42 +0800

    1179	给匈牙利加上时间选项。

--- main.cpp
+++ main.cpp
@@ -4,17 +4,24 @@
 
 #include "hungarian.hpp"
 
+using namespace std;
 
 
 int main()
 {
-    Hungarian test("../example3.txt");
+    // Hungarian test("../example3.txt");
     // Hungarian test("../example4_10x10.txt");
-    // Hungarian test("../example4_10x10.txt");
+    Hungarian test("../example5_20x20.txt");
     // std::cout << "hello" << std::endl;
+
+    int start_time = clock();
 
     test.hungarian_solve();
     Hungarian::print_pair_vector("work assignment", test.get_work_assignment());
+
+    double elapsed_time = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+    std::cout << " elapsed time(s): " << elapsed_time;
+
     // test.brute_force();
 
     return 0;
